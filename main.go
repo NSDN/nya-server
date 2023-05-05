bae6da9ea3487ec56455f50d8f3af92d0f01ed8a
SETSUIKI HYORYU <tayutai@outlook.jp>
2023-05-05 15:19:39 +0900

    feat: connect to mongodb

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/NSDN/nya-server/repositories"
 	"github.com/NSDN/nya-server/router"
+	"github.com/NSDN/nya-server/utils"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	// 从 .env 文件中加载环境变量
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal(utils.Messages.ENVIRONMENT_ERROR_NOT_FOUND)
+	}
+}
 
 func main() {
 	r := router.SetupRouter()
-	r.Run(":10127")
+	repositories.SetupDatabase()
+
+	APPLICATION_PORT := "APPLICATION_PORT"
+	port := os.Getenv(APPLICATION_PORT)
+
+	if port == "" {
+		log.Fatal(utils.Messages.ENVIRONMENT_ERROR_NEED_ONE(APPLICATION_PORT))
+	}
+
+	r.Run(port)
 }
