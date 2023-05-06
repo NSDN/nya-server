566ca0f7b75136a2c599709455cf5b035b2e3287
SETSUIKI HYORYU <tayutai@outlook.jp>
2023-05-06 14:25:31 +0900

    feat(localization-plate): build model file

--- controllers/plates/plates.go
+++ controllers/plates/plates.go
@@ -3,39 +3,28 @@ package plates
 import (
 	"net/http"
 
+	platesModels "github.com/NSDN/nya-server/models/plates"
 	"github.com/gin-gonic/gin"
 )
 
-type PlateList struct {
-	ID int `json:"id"`
-	// 显示名
-	Name string `json:"name"`
-	// 路由名
-	RouteName string `json:"routeName"`
-	// 版块名颜色（同 css）
-	TitleColor string `json:"titleColor"`
-	// 背景图片（图床地址）
-	Background string `json:"background"`
-}
-
 func GetPlateList(context *gin.Context) {
-	list := []PlateList{
+	list := []platesModels.Plate{
 		{
 			ID:         0,
-			Name:       "汉化区",
-			RouteName:  "LocalizationPlate",
-			TitleColor: "#fff",
+			Name:       "喵玉汉化馆",
+			RouteName:  "PlateLocalization",
+			NameColor:  "#fff",
 			Background: "https://static-event.benghuai.com/new_mihoyo_homepage/images/download/cg/origin/2020-10-22.jpg",
 		},
 		{
 			ID:        1,
-			Name:      "sample01",
-			RouteName: "Sample01",
+			Name:      "喵玉咏唱组",
+			RouteName: "PlateMusic",
 		},
 		{
 			ID:        2,
-			Name:      "sample02",
-			RouteName: "Sample02",
+			Name:      "魔女的茶会",
+			RouteName: "PlateChat",
 		},
 	}
 
