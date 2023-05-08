108f30db421a566d1c4d6d45b08ebff198db2d7a
SETSUIKI HYORYU <tayutai@outlook.jp>
2023-05-08 11:22:46 +0900

    refactor(plate): 重新组织版块路由

--- models/plates/plates.go
+++ models/plates/plates.go
@@ -1,13 +1,23 @@
 package plates
 
+type PageType string
+
+const (
+	COMMIC  PageType = "commic"
+	ARTICLE PageType = "article"
+)
+
 type Plate struct {
+	// 编号
 	ID int `json:"id"`
 	// 显示名
 	Name string `json:"name"`
 	// 路由名
 	RouteName string `json:"routeName"`
-	// 版块名颜色（同 css）
+	// 显示名颜色（同 css）
 	NameColor string `json:"nameColor"`
 	// 背景图片（图床地址）
 	Background string `json:"background"`
+	// 画面类型（["commic", "article"]）
+	PageType PageType `json:"pageType"`
 }
