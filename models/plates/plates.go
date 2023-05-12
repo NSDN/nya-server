dbe00d348073922f7136329baf23620b2dd964c0
SETSUIKI HYORYU <tayutai@outlook.jp>
2023-05-12 21:01:34 +0900

    refactor: 更新初始化版块列表控制器

--- models/plates/plates.go
+++ models/plates/plates.go
@@ -13,11 +13,9 @@ type Plate struct {
 	// 显示名
 	Name string `json:"name"`
 	// 路由名
-	RouteName string `json:"routeName"`
-	// 显示名颜色（同 css）
-	NameColor string `json:"nameColor"`
+	RouteName string `json:"routeName" bson:"routeName"`
 	// 背景图片（图床地址）
 	Background string `json:"background"`
 	// 画面类型（["commic", "article"]）
-	PageType PageType `json:"pageType"`
+	PageType PageType `json:"pageType" bson:"pageType"`
 }
